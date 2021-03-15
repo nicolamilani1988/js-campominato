7905a758dfe56d5e7e0c86ae3d2cc7c7f96ce535
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-03-15 14:53:50 +0100

    Scritto tutto in unica funzione

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-function getRnd (min,max){
-  var minRnd = min;
-  var maxRnd = max - minRnd + 1;
-  var numRnd = Math.floor(Math.random()*maxRnd)+minRnd;
-  return numRnd;
-}
+function getRndDifferentNumbers (min,max, totLength){
 
-
-function getDifferentNumbers (totLength){
   var arrayTot = [];
   var array = [];
-  var index = arrayTot.indexOf(getRnd(1,20));
-
   while (array.length < totLength){
+
+    var minRnd = min;
+    var maxRnd = max - minRnd + 1;
+    var numRnd = Math.floor(Math.random()*maxRnd)+minRnd;
+    var index = arrayTot.indexOf(numRnd);
+
     if(arrayTot.length<1){
-      arrayTot.push(getRnd(1,20));
+      arrayTot.push(numRnd);
     } else {
-      arrayTot.push(getRnd(1,20));
+      arrayTot.push(numRnd);
       if (index == -1){
-        array.push(getRnd(1,20))
+        array.push(numRnd)
       }
     }
   }
   return array;
+
 }
 
 
-var bombs = getDifferentNumbers(16);
+var bombs = getRndDifferentNumbers(1,100,16);
 console.log(bombs);
