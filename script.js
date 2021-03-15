d8bdce06e10330b7627ca320fde7d39cf38c9eb5
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-03-15 14:26:45 +0100

    create function per la prima parte di esercizio

--- script.js
+++ script.js
@@ -6,24 +6,26 @@ function getRnd (min,max){
 }
 
 
-
-var bombsTot = [];
-var bombs = [];
-
-while (bombs.length < 16){
-
-  var numRnd = Math.floor(Math.random()*100)+1;
-  var index = bombsTot.indexOf(numRnd);
-  if (bombsTot.length < 1){
-    bombsTot.push(numRnd);
-  } else {
-      bombsTot.push(numRnd);
-      if(index == -1){
-      bombs.push(numRnd);
+function getDifferentNumbers (arrayTot,array,tot){
+  var arrayTot = [];
+  var array = [];
+  while (array.length < tot){
+    var index = arrayTot.indexOf(getRnd(1,100));
+    if(arrayTot.length<1){
+      arrayTot.push(getRnd(1,100));
+    } else {
+      arrayTot.push(getRnd(1,100));
+      if (index == -1){
+        array.push(getRnd(1,100))
       }
     }
-
+  }
+  return array;
 }
 
-console.log(bombsTot);
-console.log (bombs);
+// var randomNumber = getRnd(1,100);
+// console.log(randomNumber);
+var bombsTot = [];
+var bombs = [];
+var bombsOk = getDifferentNumbers(bombsTot,bombs,16);
+console.log(bombsOk);
