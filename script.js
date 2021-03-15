009a5b312d2f053c4d08c22fe1a02b9a32b4836a
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-03-15 13:59:41 +0100

    create istruzioni per fare array di 16 numeri diversi

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-
-var bombs = [];
-
-for (i=0;i<16;i++){
-  var rndNum = Math.floor(Math.random()*100)+1;
-  bombs.push(rndNum);
+function getRnd (min,max){
+  var minRnd = min;
+  var maxRnd = max - minRnd + 1;
+  var numRnd = Math.floor(Math.random()*maxRnd)+minRnd;
+  return numRnd;
 }
 
-console.log(bombs);
+
+
+var bombsTot = [];
+var bombs = [];
+
+while (bombs.length < 16){
+
+  var numRnd = Math.floor(Math.random()*100)+1;
+  var index = bombsTot.indexOf(numRnd);
+  if (bombsTot.length < 1){
+    bombsTot.push(numRnd);
+  } else {
+      bombsTot.push(numRnd);
+      if(index == -1){
+      bombs.push(numRnd);
+      }
+    }
+
+}
+
+console.log(bombsTot);
+console.log (bombs);
