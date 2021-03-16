ba219799ae22a6c842eab4fd6a05554ec40dee27
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-03-16 15:57:48 +0100

    creata funzione per associare un numero all'input cliente.

--- BONUS HTML/script-html.js
+++ BONUS HTML/script-html.js
@@ -2,27 +2,28 @@
 function createCell (){
   var cella = document.getElementById("griglia");
   for(var i=0;i<100;i++){
-    cella.innerHTML += '<li class="cell"><input id="cell-'+(i+1)+'" class="number" type="checkbox" name="" data-numb='+ (i+1) + '><span class="cifra">'+(i+1)+'</span></li>';
+    cella.innerHTML += '<li class="cell"><input id="cell-'+(i+1)+'" class="number" type="checkbox" name = "" data-numb="'+ (i+1) + '"><span class="cifra">'+(i+1)+'</span></li>';
 }
 }
 
-// function selectBomb (cellNum){
-//   myBtn.addEventListener("click", function getBomb(){
-//       var myBtn = selectBomb(i+1);
-//        console.log(myBtn.dataset.numb);
-//   })
-//   var cellNum = myBtn.checked;
-//   return cellNum;
-//   }
-//
-//   var myCell = document.getElementById("cell-"+cellNum);
-//   return myCell;
-// }
 
 createCell();
 
-var myBtn = document.getElementById("cell-"+ 8);
-myBtn.addEventListener("click", function getBomb(){
+// funzione per associare input utente a un numero preciso.
+function getBomb(){
+  var lis = document.getElementsByTagName("li");
+  for(var i=0;i<lis.length;i++){
+    var li = lis[i];
 
-     console.log(myBtn.dataset.numb);
-})
+
+    li.addEventListener("click",function(){
+
+      var selectedBomb = parseInt(this.children[0].dataset.numb);
+      console.log(selectedBomb);
+
+    })
+
+  }
+}
+
+getBomb();
